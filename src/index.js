8b7b1c1c978d532597b261cba28140c1316cdd6d
Jacob Hampton <jhampton792@gmail.com>
2021-02-24 16:12:59 -0600

    Added delete button

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   addActivity()
+  deleteActivity()
 });
 
 function newActivity(str){
   const list = document.getElementById('tasks')
   const newActivity = document.createElement('li')
-  newActivity.innerText = str
+  newActivity.innerHTML = `<span>${str}</span><button type="button">Delete</button>`
   list.appendChild(newActivity)
 }
 
 function addActivity(){
-  let form = document.getElementById('create-task-form')
+  const form = document.getElementById('create-task-form')
   form.addEventListener('submit', function(event){
     event.preventDefault()
     const activityInput = event.target.newTaskDescription.value
     newActivity(activityInput)
     form.reset()
+  })
+}
+
+function deleteActivity() {
+  const list = document.getElementById('tasks')
+
+  list.addEventListener('click', function(event) {
+
+    // if (event.target.type == "button") {
+    //   event.path[1].remove()
+    // }
+
+    console.log(event)
+    if (event.target.innerText == "Delete") {
+      event.path[1].remove()
+    }
   })
 }
